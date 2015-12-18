bf7e5d3a24067dca3769fd86f050bec25b3e1f30
JotaroS <jojojoe77@gmail.com>
2015-12-18 13:39:57 +0900

    12/18 commit

--- app.js
+++ app.js
@@ -36,17 +36,6 @@ getRain : function(callback){
             parseString(body, function (err, result) {
                rain =result.weatherforecast.pref[0].area[3].info[0].rainfallchance[0].period[3]._;
                callback(null,rain);
-            /*  
-                var day = result.weatherforecast.pref[0].area[3].info[0]['$'].date + "\n";
-                var weather = result.weatherforecast.pref[0].area[3].info[0].weather[0] + "\n";
-                var detail = result.weatherforecast.pref[0].area[3].info[0].weather_detail[0] + "\n";
-                var max = "最高気温は" + result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[0]._ + "度\n";
-                var min = "最低気温は" + result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[1]._ + "度です";
-     
-                var message = "おはようございます。天気予報です\n" + day + weather + detail + max + min;
-                
-                console.log(message);
-        */
 
             });
         } else { 
@@ -54,6 +43,39 @@ getRain : function(callback){
         }
     });
     return rain;
+},
+getTemp : function(callback){
+    var rain;
+    request(url,function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            parseString(body, function (err, result) {
+               var max =result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[0]._ ;
+                var min =result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[1]._ ;
+               callback(null,max,min);
+
+            });
+        } else { 
+            console.log(error + " : " + response);
+        }
+    });
+    return rain;
+},
+getWeather : function(callback){
+    var weather;
+    request(url,function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            parseString(body, function (err, result) {
+               var w = result.weatherforecast.pref[0].area[3].info[0].weather[0] + "\n";
+               callback(null,w);
+
+            });
+        } else { 
+            console.log(error + " : " + response);
+        }
+    });
+    return weather;
 }
 
 }
--- main.js
+++ main.js
@@ -6,18 +6,27 @@ var AFGFX = require('./Adafruit_GFX.js');
 var fs = require('fs');
 var Edison = require('./Edison.js');
 var rootDir = __dirname+'/images/';
+require('date-utils');
 var imageList = [];
 var lcdTest = null;
 var ada = null;
 var picIndex = 0;
 var slideShowTimer = 0;
 
-Edison.enable_i2c6_breakout(startLCD);
+//////////////////////////////////////////////////////////////////////////////
+// feedparser - https://www.npmjs.org/package/feedparser
+var news_str = new Array(1024);
+news_str[0]="";
 
+///////////////////////////////////////////////////////////////////////////////
+
+Edison.enable_i2c6_breakout(startLCD);
+var day;
+var time;
 function startLCD()
 {
   lcdTest = new SSD1306();
-ada = new AFGFX(128,64);
+  ada = new AFGFX(128,64);
   lcdTest.init();
   lcdTest.clear();
   lcdTest.display();
@@ -28,20 +37,25 @@ ada = new AFGFX(128,64);
 
   //start the slide show
   //slideShow();
-
-    while(1)
-        drawMenu(); 
+  getNews();
+  getWeather.getRain(returnRainResult);
+  getWeather.getTemp(returnTempResult);
+  getWeather.getWeather(returnWeatherResult);
+  setInterval(function(){getWeather.getRain(returnRainResult);
+                          getWeather.getTemp(returnTempResult);
+                          getWeather.getWeather(returnWeatherResult);
+  },30000);
+    //while(1)
+  drawMenu();
+  setInterval(function(){drawMenu();},10); 
     
 }
 
 var count = 0;
 var r = 0.0;
-var str = ""
-function returnResult(err,result){
-    console.log(result);
-        str = result;
-    ada.drawString(55,50,str+"12˚C 24%",1,1,1);
-}
+var str = "fetching..."
+var temp_str="";
+var weather_str="";
 function draw(){
     lcdTest.clear();
     for(var i=0;i<1;i++)
@@ -55,27 +69,56 @@ function draw(){
     else count =0;
     //setTimeout(draw,100); //16.666[ms] = 60[fps]
 }
-
+var cnt =0;
 function drawMenu(){
     lcdTest.clear();
-    
+    var dt = new Date();
+    day = dt.toFormat("DDD MMM DD");
+    time = dt.toFormat("HH24:MI");
+    sec = dt.toFormat("SS");
+    var _min =parseFloat(dt.toFormat("MI"));
+    var _hr  =parseFloat(dt.toFormat("HH24"));
     //header
     ada.drawString(0,0,"JTRWatch",1,1,1);
     ada.drawString(100,0,"100%",1,1,1);
-    //clock
     ada.drawLine(0,10,128,10,1);
-    ada.drawCircle(28,37,18,1);
-    ada.drawLine(28,37,30,22,1);
-    ada.drawLine(28,37,35,47,1);
+    //clock
+    ada.drawCircle(25,37,18,1);
+    ada.drawLine(25,37,parseInt(25+17*Math.sin(-_min*6.0*Math.PI/180+Math.PI)),
+      parseInt(37+17*Math.cos(-_min*6.0*Math.PI/180+Math.PI)),1);
+    ada.drawLine(25,37,parseInt(25+14*Math.sin(-(_hr*30.0+_min*0.5)*Math.PI/180+Math.PI)),
+      parseInt(37+14*Math.cos(-(_hr*30.0+_min*0.5)*Math.PI/180+Math.PI)),1);
     //cal
-    ada.drawString(55,20,"Mon Dec 21",1,1,1);
+    ada.drawString(52,20,""+day,1,1,1);
     //time
-    ada.drawString(55,32,"00:20",1,2,2);
-    //Weather
-    //ada.drawString(55,50,"12˚C 24%",1,1,1);
-    getWeather.getRain(returnResult);
+    ada.drawString(52,31,""+time,1,2,2);
+    //Info max str 13
+    ada.drawString(52,50,news_str[0].substring(cnt/4,cnt/4+13)+str,1,1,1);
+    ada.drawString(113,39,""+sec,1,1,1);
+    //getWeather.getRain(returnResult);
     lcdTest.display();
-    
+    cnt++;
+}
+function drawInfo(){
+    ada.drawString(55,50,temp_str+str+"%",1,1,1);
+}
+function returnRainResult(err,result){
+    console.log("hi");
+    console.log(result);
+    str = result+"%";
+    //ada.drawString(55,50,str+"12˚C 24%",1,1,1);
+}
+function returnTempResult(err,res_h,res_l){
+    temp_str=res_l+"to"+res_h+"C ";
+}
+function returnWeatherResult(err,res){
+    console.log("weater");
+    var txt="";
+    if (res.match("くもり")) txt=  "cloudy ";
+    else if(res.match("晴れ")) txt="fine    ";
+    else if(res.match("雨")) txt = "rainy   ";
+    console.log(txt);
+    weather_str=txt;
 }
 function slideShow() {
   displayPNG(rootDir + imageList[picIndex]);
@@ -110,6 +153,41 @@ function listLogo() {
       imageList.push(fileName);
     }
   }
+}
+
+function getNews(){
+  var count =0;
+  var FeedParser = require('feedparser')
+    , request = require('request');
+
+var req = request('http://feeds.reuters.com/reuters/topNews')
+    , feedparser = new FeedParser(); // new FeedParser([options])でoptions設定
+
+req.on('error', function (error) {
+    // リクエストエラー処理
+});
+req.on('response', function (res) {
+    var stream = this;
+    if (res.statusCode != 200) {
+        return this.emit('error', new Error('Bad status code'));
+    }
+    stream.pipe(feedparser);
+});
+
+feedparser.on('error', function(error) {
+    // 通常のエラー処理
+});
+feedparser.on('readable', function() {
+    var stream = this
+        , meta = this.meta
+        , item;
+    if(item = stream.read()) {
+        // タイトルとリンクを取得
+        console.log(item.title);
+        news_str[count]=item.title;
+        count++;
+    }
+});
 }
 
 function displayPNG(fullPathToPNG) {
